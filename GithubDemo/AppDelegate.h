de9db032ec3ca0ee92a3b51a1a8f630ef57732e5
tomson <hankuikuide@163.com>
2015-04-19 11:05:08 +0800

    Initial Commit

---
+++ GithubDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GithubDemo
+//
+//  Created by Tomson on 15-4-19.
+//  Copyright (c) 2015年 Org.CTIL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GithubDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GithubDemo
+//
+//  Created by Tomson on 15-4-19.
+//  Copyright (c) 2015年 Org.CTIL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
